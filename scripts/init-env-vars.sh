db3ba090911e75b625b72aaa20a127379bcbf35a
venkatsunilm <venkatsunilminchala@gmail.com>
2024-03-22 02:38:41 +0200

    Trying to fix state file issues

--- scripts/init-env-vars.sh
+++ scripts/init-env-vars.sh
@@ -9,3 +9,6 @@ export TF_STATE_CONTAINER_NAME=tfstate
 export KEYVAULT_NAME=terraform-kv
 
 export TF_VAR_location=westeurope
+export TF_VAR_common_resource_group=terraform-rg
+export TF_VAR_storage_account=tfstate
+export TF_VAR_container=tfstate
---
+++ tf/core/variables.tf
@@ -0,0 +1,8 @@
+variable "environment" {
+  description = "Name of the environment"
+}
+
+variable "location" {
+  description = "Azure location to use"
+}
+
