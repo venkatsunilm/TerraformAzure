3eaf25bdc869b4dbd06ce35472848bb53d3b3a5c
venkatsunilm <venkatsunilminchala@gmail.com>
2024-03-22 02:36:02 +0200

    In progress: trying to fix remote state issue

--- scripts/init-env-vars.sh
+++ scripts/init-env-vars.sh
@@ -3,7 +3,9 @@
 set -e
 
 export LOCATION=westeurope
-export COMMON_RESOURCE_GROUP_NAME=terraform-ref-rg
+export COMMON_RESOURCE_GROUP_NAME=terraform-rg
 export TF_STATE_STORAGE_ACCOUNT_NAME=tfstate2195
-export TF_STATE_CONTAINER_NAME=tfstate-ref
-export KEYVAULT_NAME=terraform-ref-kv
+export TF_STATE_CONTAINER_NAME=tfstate
+export KEYVAULT_NAME=terraform-kv
+
+export TF_VAR_location=westeurope
