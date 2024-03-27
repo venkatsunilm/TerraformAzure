a0835ee046b3662b9918c3dd8f45942316b13ae0
venkatsunilm <venkatsunilm@gmail.com>
2024-03-28 00:42:52 +0200

    PR: ⬆️ : Update dependent versions (#4)

--- tf/core/variables.tf
+++ tf/core/variables.tf
@@ -1,7 +1,25 @@
 variable "location" {
-  description = "Azure location to use"
+  description = "Azure location to use for deploying resources."
+  type        = string
+  default     = "westeurope"
 }
 
+# variable "environment" {
+#   description = "The deployment environment (e.g., dev, staging, production)."
+#   type        = string
+#   validation {
+#     condition     = regex("^dev$|^staging$|^production$")
+#     error_message = "Valid values are 'dev', 'staging', or 'production'."
+#   }
+# }
+
 variable "environment" {
-  description = "environment"
+  description = "The deployment environment (e.g., dev, staging, production)."
+  type        = string
+
+  validation {
+    condition     = can(regex("^dev$|^staging$|^production$", var.environment))
+    error_message = "Invalid environment specified. Please use 'dev', 'staging', or 'production'."
+  }
 }
+
