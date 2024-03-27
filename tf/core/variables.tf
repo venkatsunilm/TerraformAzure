de490b733942c35e42dcb95cea8a430b5134cb20
venkatsunilm <venkatsunilm@gmail.com>
2024-03-27 15:24:13 +0200

    Applying complete signature for variables

--- tf/core/variables.tf
+++ tf/core/variables.tf
@@ -1,7 +1,15 @@
 variable "location" {
-  description = "Azure location to use"
+  description = "Azure location to use for deploying resources."
+  type        = string
+  default     = "westeurope"
 }
 
 variable "environment" {
-  description = "environment"
+  description = "The deployment environment (e.g., dev, staging, production)."
+  type        = string
+  validation {
+    condition     = regex("^dev$|^staging$|^production$")
+    error_message = "Valid values are 'dev', 'staging', or 'production'."
+  }
 }
+
